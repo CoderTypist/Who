bc521754a2372ef878ac8ec836a0d8be3a33076c
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2021-10-25 19:22:46 -0500

    Update whohog.sh
    Now lists item owners

--- whohog.sh
+++ whohog.sh
@@ -16,10 +16,11 @@ echo -n "" > "$out_sorted"
 for item in $(ls); do
 
     # find the size of each directory
-    if [ -d "$item" ] ; then
+    if [ -d "$item" -o -f "$item" ] ; then
+
+        # convert du output to be comma delimited
         du -h -sb "$item" | sed -E 's/\s+/,/g' >> "$out_unsorted"
     fi
-
 done
 
 # sort directories in descending order by bytes
@@ -27,49 +28,78 @@ sort -t, -r -nk1 "$out_unsorted" > "$out_sorted"
 
 # '/' needs to be converted to '\/' in order to be regular expression friendly
 target="$(echo "$target" | sed 's|/|\\/|g')"
-# df provised the number of 1024 byte units being used
+
+# df provides the number of 1024 byte units being used
 fs_1k_blocks="$(df | sed -n "/${target}$/p" | awk '{print $2}')"
+
+# get the percent of the fs that is in use
+fs_percent_used="$(df | sed -n "/${target}$/p" | awk '{print $5}')"
+# remove the '%' at the end
+fs_percent_used="${fs_percent_used:0:${#fs_percent_used}-1}"
+
 # multiply the number of 1024 byte units by 1024 to get the number of bytes
 fs_total="$(( $fs_1k_blocks * 1024 ))"
 
-# bytes used by dirs
-all_dir_totals=0
-# bytes per gb
+# bytes used by items
+byte_total=0
+# bytes per gb, where 1GB=1024*1024*1024
 bytes_per_gb=1073741824
 
 # table header
-echo "                 Dir     Gigabytes       Percent        Bytes"
-echo " --------------------------------------------------------------------"
+echo "       Gigabytes       Percent                   Bytes    Owner             Item"
+echo " ----------------------------------------------------------------------------------------------------"
 
-# directory statistics
+# item statistics
 while read -r line; do
 
-    # get directory name
+    # get item name
     name="$(echo "$line" | awk 'BEGIN{FS=","} {printf("%s", $2)}')"
-    # bytes used by the directory
-    used="$(echo "$line" | awk 'BEGIN{FS=","} {printf("%s", $1)}')"
-    # keep track of bytes used by all directories
-    all_dir_totals="$(($all_dir_totals + $used))"
-    # percentage of fs space being used by the directory
-    percentage="$(echo "scale=10;$used/$fs_total*100" | bc -l)"
-    # gb being used by the directory
-    gb="$(echo "scale=4;$used/$bytes_per_gb" | bc -l)"
+
+    # bytes used by the item
+    bytes="$(echo "$line" | awk 'BEGIN{FS=","} {printf("%s", $1)}')"
+
+    # keep track of bytes used by all items
+    byte_total="$(($byte_total + $bytes))"
+
+    # owner
+    owner="$(ls -la | sed -n "/$name$/p" | awk '{print $3}')"
+
+    # percentage of fs space being used by items
+    percentage="$(echo "scale=10;$bytes/$fs_total*100" | bc -l)"
+
+    # gb being used by the items
+    gb="$(echo "scale=4;$bytes/$bytes_per_gb" | bc -l)"
+
+    item_type=""
+    if [ -d "$name" ] ; then
+        item_type="D"
+    elif [ -f "$name" ] ; then
+        item_type="F"
+    else
+        # this should never happen
+        item_type="?"
+    fi
 
     # print directory data
-    printf "%20s    %12f    %10f    %15d\n" "$name" "$gb" "$percentage" "$used"
+    printf "%16f    %10f    %20d    %-14s    %s: %s\n" "$gb" "$percentage" "$bytes" "$owner" "$item_type" "$name"
 
 done < "$out_sorted"
 
 # overall statistics
 echo ""
-percent_accounted="$(echo "scale=10;$all_dir_totals/$fs_total*100" | bc -l )"
-percent_missing="$(echo "scale=10;100-$percent_accounted" | bc -l )"
-gb_dir="$(echo "scale=4;$all_dir_totals/$bytes_per_gb" | bc -l)"
+
+fs_percent_accounted="$(echo "scale=10;$byte_total/$fs_total*100" | bc -l )"
+fs_percent_missing="$(echo "scale=10;$fs_percent_used-$fs_percent_accounted" | bc -l )"
+fs_free="$((100-$fs_percent_used))"
+
+gb_dir="$(echo "scale=4;$byte_total/$bytes_per_gb" | bc -l)"
 gb_fs="$(echo "scale=4;$fs_total/$bytes_per_gb" | bc -l)"
-echo " Gigabytes accounted for: $gb_dir of $gb_fs"
-echo " Bytes accounted for:     $all_dir_totals of $fs_total"
-echo " Percent accounted for:   $percent_accounted"
-echo " Percent unaccounted for: $percent_missing"
+
+echo " GB accounted for:    $gb_dir of $gb_fs"
+echo " bytes accounted for: $byte_total of $fs_total"
+printf " Percent accounted for:   %.2f%%\n" "$fs_percent_accounted"
+printf " Percent unaccounted for: %.2f%%\n" "$fs_percent_missing"
+echo " Free storage: ${fs_free}%"
 echo ""
 
 # clean up
